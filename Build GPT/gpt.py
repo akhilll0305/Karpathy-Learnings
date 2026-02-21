3386fba9a54209174db0617712e5869042a4d406
Akhil0305 <r.akhilreddy007@gmail.com>
2026-02-21 12:34:32 +0530

    Learnt about Feed forward network implementation

--- Build GPT/gpt.py
+++ Build GPT/gpt.py
@@ -104,24 +104,22 @@ class MultiHeadAttention(nn.Module):
         return out
 
 
+class FeedFoward(nn.Module):
+    """ a simple linear layer followed by a non-linearity """
 
-
-class Block(nn.Module):
-    """ Transformer block: communication followed by computation """
-
-    def __init__(self, n_embd, n_head):
-        # n_embd: embedding dimension, n_head: the number of heads we'd like
+    def __init__(self, n_embd):
         super().__init__()
-        head_size = n_embd // n_head
-        self.sa = MultiHeadAttention(n_head, head_size)
-        self.ffwd = FeedFoward(n_embd)
-        self.ln1 = nn.LayerNorm(n_embd)
-        self.ln2 = nn.LayerNorm(n_embd)
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout),
+        )
 
     def forward(self, x):
-        x = x + self.sa(self.ln1(x))
-        x = x + self.ffwd(self.ln2(x))
-        return x
+        return self.net(x)
+
+
 
 class GPTLanguageModel(nn.Module):
 
